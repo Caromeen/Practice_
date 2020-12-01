5a82463a1e55e4c35b6947b117a91c0f64c6c5d5
BorodichNI <borodich_ni_17@mf.grsu.by>
2020-12-01 13:37:14 +0300

    почти

--- src/Pyramid.java
+++ src/Pyramid.java
@@ -1,24 +1,65 @@
-import java.util.Scanner;
-
 public class Pyramid {
+
     public static void main(String[] args) {
 
-        Scanner enter = new Scanner(System.in);
+        int n = 9;
+        int array[][] = new int[n][n];
 
-        int n;
+        int row = 0;
+        int col = 0;
+        int step = 0;
+        int i = 0;
+        int x = 1;
+        int c = 1;
+        int v = 1;
+        int a = 1;
 
-        System.out.println("Введите размерность пирамиды");
-        n = enter.nextInt();
 
-        for (int i = 0; i <= n; i++) {
-            for (int j = 1; j <= n; j++) {
-                if (i >= j) {
-                    System.out.print(j);
-                } else {
-                    System.out.print(" ");
-                }
+        for (int z = 0; i < n * n - 1; z++) {
+
+            for (int j = 0; j < n - step; j++) {
+                array[row][col++] = x;
+                i++;
+            }
+            x++;
+            col--;
+            ++step;
+
+            for (int k = 0; k < n - step; k++) {
+                array[++row][col] = c;
+                i++;
+            }
+            c++;
+
+            for (int l = 0; l < n - step; l++) {
+                array[row][--col] = v;
+                i++;
+            }
+            v++;
+            ++step;
+
+            for (int m = 0; m < n - step; m++) {
+                array[--row][col] = a;
+                i++;
+            }
+            a++;
+            col++;
+//            if (n % 2 != 0) {
+//                for (int b = 0; b < n - step; b++) {
+//                    array[row][col++] = n;
+//                }
+//            }
+        }
+
+        for (int d = 0; d < n; d++) {
+            for (int s = 0; s < n; s++) {
+                System.out.print(array[d][s] + "\t");
             }
             System.out.println();
         }
+
+
     }
 }
+
+
